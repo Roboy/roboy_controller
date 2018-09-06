9318c77cd97a3ee7e097552b50b2fb883c5b20e3
letrend <simon.trendel@tum.de>
2018-09-06 11:47:54 +0200

    with head and aruco marker tracking

--- src/CupTablePlugin.cpp
+++ src/CupTablePlugin.cpp
@@ -1,41 +1,40 @@
-
 #include <gazebo/gazebo.hh>
 #include <gazebo/physics/Model.hh>
-#include <iostream>
 #include <gazebo/physics/physics.hh>
-#include <gazebo/common/common.hh>
-#include <ignition/math/Vector3.hh>
-#include <functional>
-#include <boost/bind.hpp>
 #include <boost/algorithm/string.hpp>
-#include <gazebo/transport/transport.hh>
-#include <gazebo/msgs/msgs.hh>
-#include <thread>
 #include "ros/ros.h"
 #include "ros/callback_queue.h"
-#include "ros/subscribe_options.h"
 #include "std_msgs/String.h"
 #include "geometry_msgs/Vector3.h"
-#include "geometry_msgs/Vector3.h"
-
-#include <ignition/math/Pose3.hh>
-#include <ignition/math/Quaternion.hh>
-
-//TF stuff
 #include <tf/tf.h>
 #include <tf/transform_broadcaster.h>
 #include <tf/transform_listener.h>
 #include <common_utilities/rviz_visualization.hpp>
+#include <sensor_msgs/Image.h>
+#include <image_transport/image_transport.h>
+#include <cv_bridge/cv_bridge.h>
+#include <opencv2/aruco.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/calib3d/calib3d.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <Eigen/Dense>
+#include <vector>
 
+static const std::string OPENCV_WINDOW = "Image window";
 using namespace gazebo;
+using namespace cv;
+using namespace Eigen;
+using namespace std;
 
 class CupTablePlugin : public ModelPlugin, public rviz_visualization {
 
 public:
     CupTablePlugin() : ModelPlugin() {
-
     }
-
+    ~CupTablePlugin(){
+        cv::destroyWindow(OPENCV_WINDOW);
+    }
 public:
     void Load(physics::ModelPtr _model, sdf::ElementPtr /*_sdf*/) {
         this->model = _model;
@@ -51,17 +50,46 @@ public:
         boost::shared_ptr<ros::AsyncSpinner> spinner = boost::shared_ptr<ros::AsyncSpinner>(new ros::AsyncSpinner(1));
         spinner->start();
 
+        camera_sub = nh->subscribe("/zed/camera/left/image_raw",1, &CupTablePlugin::leftCameraCB, this);
+
         this->updateConnection = event::Events::ConnectWorldUpdateBegin(
                 boost::bind(&CupTablePlugin::OnUpdate, this, _1));
 
 
         this->model->SetGravityMode(0);
-        ROS_WARN("hit detection ready");
+
+        camMatrix = Mat(3,3,CV_32FC1,K);
+        distCoeffs = Mat (1,5,CV_32FC1,D);
+
+        detectorParams = aruco::DetectorParameters::create();
+        detectorParams->cornerRefinementMaxIterations = 100;
+        dictionary = aruco::getPredefinedDictionary(aruco::PREDEFINED_DICTIONARY_NAME(cv::aruco::DICT_ARUCO_ORIGINAL));
+
+        cv::namedWindow(OPENCV_WINDOW);
+        ROS_WARN("cup table plugin ready");
     }
 
+    void leftCameraCB(const sensor_msgs::Image::ConstPtr &msg){
+        try
+        {
+            cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+        }
+        catch (cv_bridge::Exception& e)
+        {
+            ROS_ERROR("cv_bridge exception: %s", e.what());
+            return;
+        }
+    }
 
 public:
     void OnUpdate(const common::UpdateInfo & /*_info*/) {
+        if(cv_ptr!= nullptr)
+            detectAruco();
+        cv::waitKey(1);
+
+        if((ros::Time::now()-last_published).toSec()<0.01)
+            return;
+        last_published = ros::Time::now();
         int message_counter = 1000;
         for(auto link:model->GetLinks()) {
             math::Pose pose = link->GetWorldPose();
@@ -91,7 +119,7 @@ public:
             mesh.pose.orientation.z = pose.rot.z;
             mesh.pose.orientation.w = pose.rot.w;
             char meshpath[200];
-            sprintf(meshpath, "package://roboy_models/%s/meshes/CAD/%s.stl",
+            sprintf(meshpath, "package://roboy_models/%s/meshes/CAD/%s.dae",
                     model->GetName().c_str(), link->GetName().c_str());
             mesh.mesh_resource = meshpath;
             visualization_pub.publish(mesh);
@@ -103,12 +131,52 @@ public:
         }
 
     }
+
+    void detectAruco(){
+        vector< int > ids;
+        vector< vector< Point2f > > corners, rejected;
+        vector< Vec3d > rvecs, tvecs;
+
+        // detect markers and estimate pose
+        aruco::detectMarkers(cv_ptr->image, dictionary, corners, ids, detectorParams, rejected);
+        aruco::estimatePoseSingleMarkers(corners, markerLength, camMatrix, distCoeffs, rvecs, tvecs);
+        // draw results
+        if(ids.size() > 0) {
+            aruco::drawDetectedMarkers(cv_ptr->image, corners, ids);
+            for(unsigned int i = 0; i < ids.size(); i++) {
+                if(!arucoIDs.empty()) {
+                    if (std::find(arucoIDs.begin(), arucoIDs.end(), ids[i]) == arucoIDs.end())
+                        continue;
+                }
+                aruco::drawAxis(cv_ptr->image, camMatrix, distCoeffs, rvecs[i], tvecs[i],
+                                markerLength * 0.5f);
+                double theta = sqrt(pow(rvecs[i][0],2.0) + pow(rvecs[i][1],2.0) + pow(rvecs[i][2],2.0));
+                Quaterniond q(rvecs[i][0]/theta,rvecs[i][0]/theta,rvecs[i][0]/theta,theta);
+                q.normalize();
+                tf::Transform trans;
+                trans.setRotation(tf::Quaternion(q.x(), q.y(), q.z(), q.w()));
+                trans.setOrigin(tf::Vector3(tvecs[i][0], tvecs[i][1], tvecs[i][2]-1)); // TODO camera should have its own link, so we dont need to correct the pose here
+                char str[100];
+                sprintf(str,"cup_%d",ids[i]);
+                tf_broadcaster.sendTransform(
+                        tf::StampedTransform(trans, ros::Time::now(), "table", str));
+            }
+        }
+        cv::imshow(OPENCV_WINDOW, cv_ptr->image);
+    }
 private:
     event::ConnectionPtr updateConnection;
     physics::ModelPtr model;
     ros::NodeHandlePtr nh;
-
-
+    ros::Time last_published;
+    ros::Subscriber camera_sub;
+    cv_bridge::CvImagePtr cv_ptr;
+    Mat camMatrix, distCoeffs;
+    Ptr<aruco::DetectorParameters> detectorParams;
+    Ptr<aruco::Dictionary> dictionary;
+    vector<int> arucoIDs;
+    float markerLength = 0.07f;
+    float K[9] = {687.8062263328862, 0.0, 320.5, 0.0, 687.8062263328862, 240.5, 0.0, 0.0, 1.0}, D[5] = {0,0,0,0,0};
 private:
     tf::TransformListener tf_listener;
     tf::TransformBroadcaster tf_broadcaster;
